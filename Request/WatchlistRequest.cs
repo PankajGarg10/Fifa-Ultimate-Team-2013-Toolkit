27776650df760cdcd41b93c1151c492c546a568e
Morten Trydal <trydis@hotmail.com>
2013-07-06 19:08:56 +0200

    Removed some using statements

--- Request/WatchlistRequest.cs
+++ Request/WatchlistRequest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Threading;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using UltimateTeam.Toolkit.Model;
 using UltimateTeam.Toolkit.Constant;
 
